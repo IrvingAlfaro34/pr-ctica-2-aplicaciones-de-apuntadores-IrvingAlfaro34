db83d65710253b8501e3bd15906de5744eaf0d91
Alfaro Carreto Irving Alberto <irvingalbertoalfaro01@gmail.com>
2025-03-01 15:04:37 -0600

    ejercicio7.c

--- ejercicio7.c
+++ ejercicio7.c
@@ -4,5 +4,11 @@
 int main() {
     int arr[5] = {10, 20, 30, 40, 50};
     // Código para imprimir direcciones de memoria aquí
+      printf("Direcciones de memoria de los elementos del arreglo:\n");
+    for (int i = 0; i < 5; i++)
+        {
+        printf("arr[%d] = %p\n", i, &arr[i]);
+    }
+    
     return 0;
 }
